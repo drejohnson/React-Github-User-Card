ce4fc2dd5c3720a90a783c20faad9e468e273853
DeAndre Johnson <dre@phreshr.com>
2019-10-30 07:48:27 +0800

    add followers

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import styled from 'styled-components/macro';
 import logo from './logo.svg';
 import GlobalStyle from './style/Global';
 import UserCard from './components/UserCard';
+
+const CardList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  grid-gap: 1rem;
+`;
 
 class App extends Component {
   constructor() {
@@ -10,17 +17,38 @@ class App extends Component {
     this.state = {
       user: 'drejohnson',
       data: null,
+      followers: [],
     };
   }
 
   async componentDidMount() {
-    await this.fetchUser(this.state.user);
+    const followers = await this.fetchFollowers(this.state.user);
+    this.setState({
+      data: await this.fetchUser(this.state.user),
+    });
+    this.setState({
+      followers: await this.fetchFollowers(this.state.user),
+    });
+    followers.map(user =>
+      this.fetchUser(user).then(x => {
+        this.setState({
+          followers: this.state.followers.concat(x),
+        });
+      }),
+    );
   }
 
   fetchUser = async user => {
     const result = await axios.get(`https://api.github.com/users/${user}`);
-    this.setState({
-      data: result.data,
+    return await result.data;
+  };
+
+  fetchFollowers = async user => {
+    const result = await axios.get(
+      `https://api.github.com/users/${user}/followers`,
+    );
+    return result.data.map(follower => {
+      return follower.login;
     });
   };
 
@@ -30,6 +58,15 @@ class App extends Component {
         <GlobalStyle />
         <div className='App'>
           <UserCard user={this.state.data} />
+          <CardList>
+            {this.state.followers.map(follower => {
+              return follower.avatar_url &&
+                follower.location &&
+                follower.bio ? (
+                <UserCard key={follower.name} user={follower} />
+              ) : null;
+            })}
+          </CardList>
         </div>
       </>
     );
