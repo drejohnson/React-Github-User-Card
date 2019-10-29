390f49f148cabbd67ad735a191a4cfaeb7aff6d8
DeAndre Johnson <dre@phreshr.com>
2019-10-30 04:54:21 +0800

    fetch github user

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './style/Global';
+import UserCard from './components/UserCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: 'drejohnson',
+      data: null,
+    };
+  }
+
+  async componentDidMount() {
+    await this.fetchUser(this.state.user);
+  }
+
+  fetchUser = async user => {
+    const result = await axios.get(`https://api.github.com/users/${user}`);
+    this.setState({
+      data: result.data,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <div className='App'>
+          <UserCard user={this.state.data} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
